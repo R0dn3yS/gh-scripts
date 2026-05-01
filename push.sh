c062c0ef23ca1ee4fbfb3598b63d7faccbe83dab
Rodney van den Velden <rodneymichael2002@gmail.com>
2026-05-01 11:42:59 +0200

    Advancely CI/CD done

---
+++ push.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+cd "aur/$1" || exit
+
+updpkgsums
+makepkg --printsrcinfo > .SRCINFO
+
+git add .
+git commit -m "v$2"
+git push
+
+rm -rf Advancely Advancely.zip advancely.png advancely.desktop launcher LICENSE
---
+++ src/mod.ts
@@ -0,0 +1,44 @@
+import { github } from '@roka/github';
+
+const pkgList = [
+  'advancely',
+  'advancely-bin'
+]
+
+const curVersion = Deno.readTextFileSync(`aur/${pkgList[0]}/PKGBUILD`).split('\n')[3].split('=')[1];
+
+async function getLatestVer() {
+  const repo = github().repos.get('LNXSeus', 'Advancely');
+  const releases = await repo.releases.list();
+
+  return releases[0].tag.substring(1);
+}
+
+async function updatePackages(list: string[], version: string) {
+  for (const pkg of list) {
+    let PKGBUILD = Deno.readTextFileSync(`aur/${pkg}/PKGBUILD`);
+
+    PKGBUILD = PKGBUILD.replace(curVersion, newVersion);
+
+    Deno.writeTextFileSync(`aur/${pkg}/PKGBUILD`, PKGBUILD);
+
+    const command = new Deno.Command('./push.sh', {
+      args: [
+        pkg,
+        version
+      ],
+      stdin: "null",
+      stdout: "piped"
+    });
+
+    const child = command.spawn();
+
+    const _status = await child.status;
+  }
+}
+
+const newVersion = await getLatestVer();
+
+if (newVersion !== curVersion) {
+  updatePackages(pkgList, newVersion);
+}
