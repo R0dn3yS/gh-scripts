de7fa9c0953f2657855b049bb9bf9012994e6fba
Rodney van den Velden <rodneymichael2002@gmail.com>
2026-05-11 04:22:49 +0200

    Change files to be removed

--- push_advancely.sh
+++ push_advancely.sh
@@ -9,4 +9,4 @@ git add .
 git commit -m "ci/cd release: v$2"
 git push
 
-rm -rf Advancely Advancely.zip advancely.png advancely.desktop launcher LICENSE
+rm -rf Advancely Advancely.zip advancely.png advancely.desktop launcher LICENSE pkg src
